863b840515b2144d70c3faf92a98c363ed6a4d3b
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-04-29 02:05:18 -0700

    alx

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,15 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- * main - prints the number of arguments.
- * @argc: argument count
- * @argv: Argument vector for values
- *
- *
- * Return: Always to 0
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
  */
+
+
 int main(int argc, char *argv[])
 {
-	(void)argv;
-	argc--;
-	printf("%d\n", argc);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
