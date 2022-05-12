00b42cc952e1bffc0bc1e9bc9a50d1d6f4d52a9b
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-11 17:04:43 -0700

    alx

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,8 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+/*copy list from task*/
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
