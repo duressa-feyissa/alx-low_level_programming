17f473b27ebdc9189c7714d66627b87dc82e0d35
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-17 04:55:06 -0700

    alx

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
