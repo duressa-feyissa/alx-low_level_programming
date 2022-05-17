cb89655ccff75e4e57594a9edf88d4405fbfc2b9
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-17 06:33:31 -0700

    alx

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - a function that creates a new dog.
+ * @name: Pointer to name member
+ * @age: float data type member of struct dog
+ * @owner: Pointer to onwer member
+ *
+ * Return: Point to user.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *user;
+
+	user = malloc(sizeof(dog_t));
+	if (user == NULL)
+		return (NULL);
+	user->name = name;
+	user->age = age;
+	user->owner = owner;
+
+	return (user);
+}
