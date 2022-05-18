de583d85e1fe4f7e9efbb5ffc5dca93286a85177
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-18 09:53:59 -0700

    alx

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - a function that frees dogs.
+ * @d: Pointer to struct
+ *
+ * Return: Void
+ */
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
