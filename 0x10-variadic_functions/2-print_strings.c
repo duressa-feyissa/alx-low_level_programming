6e5ba911dae53d123f249414e389ff8a24d60131
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-20 01:51:50 -0700

    alx

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(ap, char*));
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
