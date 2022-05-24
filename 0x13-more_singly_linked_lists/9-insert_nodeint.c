3f46280e1fc8ad45f6e748ef4854bfb01cc15a2b
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-24 12:42:00 -0700

    alx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *ptr, *temp, *new;
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
 	ptr = malloc(sizeof(listint_t));
 	if (ptr == NULL)
 		return (NULL);
