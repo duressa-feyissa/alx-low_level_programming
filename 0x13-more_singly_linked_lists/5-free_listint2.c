a280ab3f3937b15280f674a21ca0b6ba911f2aa0
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-24 09:57:26 -0700

    alx

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 -  a function that frees a listint_t list.
+ * @head: Pointer to struct node.
+ *
+ * Return: Void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	ptr = *head;
+	if (ptr)
+	{
+		if (ptr->next)
+		{
+			ptr = ptr->next;
+			free_listint2(&ptr);
+		}
+		free(ptr);
+		free(*head);
+	}
+	head = NULL;
+}
