7c21feaafab047c01a7184a7008b56eefce730c5
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-26 14:16:31 -0700

    alx

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,15 +12,15 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int a;
 	int sum;
 
-        sum = 0;
+	sum = 0;
 	a = n ^ m;
-        while (a > 0)
-        {
-                if ((a & 1) == 1)
-                {
-                        sum++;
-                }
-                a = a >> 1;
-        }
-        return (sum);
+	while (a > 0)
+	{
+		if (a != (a >> 1) * 2)
+		{
+			sum++;
+		}
+		a = a >> 1;
+	}
+	return (sum);
 }
