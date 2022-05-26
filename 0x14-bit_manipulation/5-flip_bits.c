83ebfdbc3b6f9a66a39587c6c04b0d1ab2bb03ad
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-26 13:42:55 -0700

    alx

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - A function count number of bits you would need to flip.
+ * @n: number
+ * @m: number
+ * Return: the number of bits flipped.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int a, i, sum;
+
+	a = (n ^ m);
+	sum = 0;
+	for (i = a; a != 0; i++)
+	{
+		if (a != ((a >> 1) * 2))
+		{
+			sum += 1;
+		}
+		a = a >> 1;
+	}
+	return (sum);
+}
