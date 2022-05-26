5939771f2423393b8067394ded075371a1904e6d
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-25 19:47:28 -0700

    alx

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,7 +3,37 @@
 #include <stdio.h>
 #include "lists.h"
 
-listint_t *find_start_loop(const listint_t *);
+/**
+ * meet - function that finds the start of the loop of the linked list
+ *
+ * @head: pointer to listint_t struct
+ *
+ * Return: node address where the loop starts
+ */
+listint_t *meet(const listint_t *head)
+{
+	listint_t *fast, *slow;
+
+	fast = (void *)head;
+	slow = (void *)head;
+
+	while (fast->next && fast->next->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = (void *)head;
+			while (fast != slow)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
+}
 
 /**
  * print_listint_safe - function that prints a listint_t linked list
@@ -12,57 +42,27 @@ listint_t *find_start_loop(const listint_t *);
  *
  * Return: nodes number
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	int count = 0, repeat = 0;
-	listint_t *start_loop;
+	int i = 0, count = 0;
+	listint_t *start;
 
 	if (head == NULL)
 		return (0);
 
-	start_loop = find_start_loop(head);
-	while (head != NULL)
+	start = meet(head);
+	while (head)
 	{
-		if (head == start_loop)
-			repeat++;
-		if (repeat > 1)
+		if (head == start)
+			i++;
+		if (i == 2)
 			break;
-		count++;
 		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
 		head = head->next;
 	}
-	if (start_loop != NULL)
-		printf("-> [%p] %d\n", (void *)start_loop, start_loop->n);
+	if (start)
+		printf("-> [%p] %d\n", (void *)start, start->n);
 	return (count);
-}
-/**
- * find_start_loop - function that finds the start of the loop
- * of the linked list
- *
- * @head: pointer to listint_t struct
- *
- * Return: node address where the loop starts
- */
-listint_t *find_start_loop(const listint_t *head)
-{
-	listint_t *ptr_t = (void *)head, *ptr_h = (void *)head;
-
-	while (ptr_h->next != NULL && ptr_h->next->next != NULL)
-	{
-		ptr_t = ptr_t->next;
-		ptr_h = ptr_h->next->next;
-
-		if (ptr_h == ptr_t)
-		{
-			ptr_t = (void *)head;
-			while (ptr_t != ptr_h)
-			{
-				ptr_t = ptr_t->next;
-				ptr_h = ptr_h->next;
-			}
-			return (ptr_h);
-		}
-	}
-
-	return (NULL);
 }
