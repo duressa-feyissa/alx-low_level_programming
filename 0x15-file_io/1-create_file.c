6edce126e0b69441232e05cd20ebb527629ba9a9
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-30 15:40:01 -0700

    alx

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - A  function that creates a file.
+ * @filename: is the name of the file to create.
+ * @text_content: is a NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, t;
+	char *s = "";
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+		text_content = s;
+
+	i = 0;
+	while (*(text_content + i))
+		i++;
+
+	t = write(fd, text_content, i);
+	if (t == -1)
+		return (-1);
+	return (1);
+}
