4f570a48d48490c60219aa26c2a7a08d37609762
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-30 14:49:22 -0700

    alx

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * read_textfile - A function that reads a text file and prints it.
+ * @filename: The file that opened or read.
+ * @letters:  letters is the number of letters it should read and print.
+ * Return: The actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, s, t;
+	char *ptr;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (0);
+
+	ptr = malloc((sizeof(char)) * letters);
+	if (!ptr)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	s = read(fd, ptr, letters);
+	if (s == -1)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	ptr[s] = '\0';
+
+	t = write(STDOUT_FILENO, ptr, letters);
+	if (t == -1)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	free(ptr);
+	return (t);
+}
