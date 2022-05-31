c863413b0868d137c924f9fc40cc1a701855efda
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-05-30 18:35:53 -0700

    alx

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,27 +15,27 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "cp file_from file_to\n");
 		exit(97);
 	}
 
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 < 0)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 	exit(98);
 	}
-	s = read(fd1, ptr, 1024);
+	s = read(fd1, ptr, BUFSIZ);
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	t = write(fd2, ptr, s);
 	if (t != s)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	a = close(fd1);
