4cc987dc0ea8f0d979804725daa116febd5a1cd4
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-07 15:02:44 -0700

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new;
 		return (new);
 	}
-	while (ptr && i <= idx) /* To add at the middle*/
+	while (ptr) /* To add at the middle*/
 	{
 		if (i == idx)
 		{
