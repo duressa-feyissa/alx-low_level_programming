9ac0737234ad53f2e33bcd363d9825abb55a7dab
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-30 12:40:26 -0700

    alx

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,12 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ptr = malloc(sizeof(hash_table_t));
 	if (ptr == NULL)
-		return NULL;
+		return (NULL);
 
 	ptr->size = size;
 	ptr->array = malloc(sizeof(hash_node_t *) * size);
 	if (ptr->array == NULL)
-		return NULL;
+		return (NULL);
 
-	return ptr;
+	return (ptr);
 }
