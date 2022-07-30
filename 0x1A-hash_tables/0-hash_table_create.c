62a502a8c53d8d198a3fd1bfd575e668bddaf542
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-30 12:38:24 -0700

    alx

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function that creates a hash table.
+ *
+ * @size: Size of the hash table
+ * Return: a pointer to the newly created hash table or NULL.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ptr;
+
+	ptr = malloc(sizeof(hash_table_t));
+	if (ptr == NULL)
+		return NULL;
+
+	ptr->size = size;
+	ptr->array = malloc(sizeof(hash_node_t *) * size);
+	if (ptr->array == NULL)
+		return NULL;
+
+	return ptr;
+}
