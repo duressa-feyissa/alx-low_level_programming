04720e487bf6c2849b3550641e8a3f655071d77e
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-30 19:47:58 -0700

    alx

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,23 +12,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int num;
 	char *temp;
-
-	if (ht == NULL || ht->array == NULL || key == NULL)
-		return (0);
+	hash_node_t *ptr = NULL;
 
 	temp = (char *)key;
-
-	if (strlen(temp) == 0)
+	if (strlen(temp) == 0 && ht == NULL && ht->array && key == NULL)
 		return (0);
 
 	num = key_index((unsigned char *)key, ht->size);
+	ptr = ht->array[num];
+	while (ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+		{
+			free(ptr->value);
+			ptr->value = strdup(value);
+			return (1);
+		}
+		ptr = ptr->next;
+	}
 	ht->array[num] = malloc(sizeof(hash_node_t));
 	if (ht->array[num] == NULL)
 		return (0);
-
-	ht->array[num]->key = temp;
+	ht->array[num]->key = strdup(key);
 	ht->array[num]->value = strdup(value);
-	ht->array[num]->next = NULL;
-
+	ht->array[num]->next = ht->array[num];
 	return (1);
 }
