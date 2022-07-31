60f29daa1aa5debe70413fbed909eee26a3fbe90
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-30 20:12:44 -0700

    alx

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,7 +15,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	num = key_index((unsigned char *)key, ht->size);
 
-	if (ht->array[num] != NULL)
+	if (ht->array[num] != NULL && strcmp(ht->array[num]->key, key) == 0)
 		return (ht->array[num]->value);
 	return (NULL);
 }
