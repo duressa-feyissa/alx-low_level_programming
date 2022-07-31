d0c52399040c8f9282a20ae69dd74e9fbac1fc50
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-30 20:06:32 -0700

    alx

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - A function that prints a hash table.
+ * hash_table_get - A function that prints a hash table.
  * @ht: is the hash table
  * @key: is a key
  * Return: On succes value of the key otherwise NULL
@@ -10,8 +10,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int num;
 
-	if (ht == NULL)
-		return NULL;
+	if (ht == NULL && key == NULL)
+		return (NULL);
 
 	num = key_index((unsigned char *)key, ht->size);
 
