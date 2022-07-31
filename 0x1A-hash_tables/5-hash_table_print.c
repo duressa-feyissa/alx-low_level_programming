1992345543ee9f0ea81ead1759f54044147a8e67
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-30 21:57:24 -0700

    alx

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,7 +4,7 @@
  * hash_table_print - A function that prints a hash table.
  * @ht:  is the hash table
  * Return: Void.
- *
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
