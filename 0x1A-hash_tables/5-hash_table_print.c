63571c37f8a953e846df2d08b48065a87c5e5ec7
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-30 21:56:00 -0700

    alx

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - A function that prints a hash table.
+ * @ht:  is the hash table
+ * Return: Void.
+ *
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int count = 0;
+	hash_node_t *ptr;
+
+	if (ht == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			if (count != 0)
+			    printf(", ");
+
+			ptr = ht->array[i];
+			while (ptr)
+			{
+				printf("'%s': '%s'", ptr->key, ptr->value);
+				ptr = ptr->next;
+				if (ptr)
+					printf(", ");
+			}
+			count = 1;
+		}
+	}
+	printf("}\n");
+}
