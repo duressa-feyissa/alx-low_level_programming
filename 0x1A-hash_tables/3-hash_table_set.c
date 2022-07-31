bad21e4bdc98fdbdc2ed0110e0521d324616bf26
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-07-30 20:23:38 -0700

    alx

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *ptr = NULL;
 
 	temp = (char *)key;
-	if (strlen(temp) == 0 && ht == NULL && ht->array && key == NULL)
+	if (strlen(temp) == 0 && !ht && !ht->array && !key && !value)
 		return (0);
 
 	num = key_index((unsigned char *)key, ht->size);
