2fe8d9d54397b7ecb3056d8accb8de5b37cd50a6
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-11-03 03:23:09 +0300

    alx

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,11 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef __SEARCH_ALGOS_H__
+#define __SEARCH_ALGOS_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-
+/*==== My function Prototypes ====*/
 int linear_search(int *array, size_t size, int value);
-void print_array(int *array, size_t l, size_t r);
 int binary_search(int *array, size_t size, int value);
-int binary_S(int *array, size_t l, size_t r, int value);
-
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif
